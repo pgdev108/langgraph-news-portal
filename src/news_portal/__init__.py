4c85c05e3a920597878064a955b5628d6f020fca
pgdev108 <pgdev108@gmail.com>
2025-10-14 18:43:06 -0700

    Add initial set of files for 5 subtopic and 2 News portal each

---
+++ src/news_portal/__init__.py
@@ -0,0 +1,13 @@
+#  -------------------------------------------------------------------------------------------------
+#   Copyright (c) 2016-2025.  SupportVectors AI Lab
+#   This code is part of the training material and, therefore, part of the intellectual property.
+#   It may not be reused or shared without the explicit, written permission of SupportVectors.
+#
+#   Use is limited to the duration and purpose of the training at SupportVectors.
+#
+#   Author: SupportVectors AI Training Team
+#  -------------------------------------------------------------------------------------------------
+# src/news_portal/__init__.py
+# Keep package imports side-effect free. Do NOT load config files here.
+__all__ = ["config", "tools", "agents", "graph", "main"]
+
